51bc972631d5d768ba5a0185954448c0c66b8bd3
Cera <cerasamson@gmail.com>
2020-06-26 04:03:32 -0700

    buttons

--- gui.py
+++ gui.py
@@ -4,17 +4,17 @@ import config
 import json
 
 def formatData(data):
-    
     endstr = ''
-
     try:
         for business in data['businesses']:
             businessInfo = ' %s \n Rating: %d \n \n' %(business['name'], business['rating'])
             endstr += businessInfo
+    
     except:
         return "Invalid location, please re-enter."
-        
+    
     return endstr
+
 
 def fetchData(userInput):
     # create object
@@ -32,23 +32,28 @@ def fetchData(userInput):
  
     # converting to json string
     dataString = json.dumps(data)
-
     label['text'] = formatData(data)
 
+def forward():
+    return
+
+
+def prev():
+    return
 
 root = tk.Tk()
 
 title = tk.Label(root, text="Boba Tracker", font='24')
-title.pack()
+title.grid(row=0, column=0, columnspan=3)
 
 canvas = tk.Canvas(root, height=720, width=1200, bg='#f7e3af')
-canvas.pack()
+canvas.grid(row=2, column=1)
 
 frame = tk.Frame(root, bg='#f7e3af')
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font='12')
-entry.place(relwidth=0.55, relheight=0.4)
+entry.place(relwidth=0.5, relheight=0.4)
 
 button = tk.Button(frame, text="Search", font=12, command=lambda: fetchData(entry.get()))
 button.place(relx=0.55, relheight=0.4, relwidth=0.2)
@@ -56,8 +61,19 @@ button.place(relx=0.55, relheight=0.4, relwidth=0.2)
 lower_frame = tk.Frame(root, bd=5)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.65, anchor='n')
 
+button = tk.Button(frame, text="Export Data", font=12)
+button.place(relx=0.55, rely=0.55, relheight=0.4, relwidth=0.2)
+
+
+buttonPrev = tk.Button(root, text="<<", command=prev)
+buttonMid = tk.Button(root, text="Exit", command=root.quit)
+buttonForward = tk.Button(root, text=">>", command=lambda: forward(2))
+
+buttonPrev.grid(row=3, column=0)
+buttonMid.grid(row=3, column=1)
+buttonForward.grid(row=3, column=2)
 
 label = tk.Label(lower_frame, bg='#f3eec3')
-label.place(relwidth=1, relheight=1)
+label.pack(relwidth=1, relheight=1)
 
 root.mainloop()
